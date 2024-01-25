cfc32399f5230af707eacd4b6da0491b22995af9
abdulazizbek001 <olimjonovabdulazizbek001@gmail.com>
2024-01-25 12:35:20 +0500

    JavaScript

---
+++ app.js
@@ -0,0 +1,11 @@
+const arr = ['Uzbekiston', 'Falastin', 'Arabiston', 'Rassiya', 'Xitoy'];
+const arr2 = ['Amerika', 'Canada', 'Karea', 'Qozog`iston', 'Yaponiya'];
+const arr3 = ['Word', 'Excel', 'Power point', 'Canva', 'Vscode'];
+const arr4 = ['Matematika', 'Fizika', 'Algebra', 'Adabiyot', 'Kimyo'];
+const arr5 = ['Frontend', 'Backend', 'Fullstack', 'CMS', 'Android'];
+
+console.log(arr);
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
+console.log(arr5);
